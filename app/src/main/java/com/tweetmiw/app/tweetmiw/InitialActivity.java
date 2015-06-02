d31ea82cc1eaa74acaa99a4c9a9ff5205f1b93b2
Salvador Garcia <salgaral@dekuwebs.com>
2015-06-02 16:08:21 +0200

    Menú implementado y arreglado los fallos

--- app/src/main/java/com/tweetmiw/app/tweetmiw/InitialActivity.java
+++ app/src/main/java/com/tweetmiw/app/tweetmiw/InitialActivity.java
@@ -1,6 +1,6 @@
 package com.tweetmiw.app.tweetmiw;
 
-import android.app.ActionBar;
+import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -10,13 +10,44 @@ import android.view.MenuItem;
 
 public class InitialActivity extends AppCompatActivity {
 
+    ViewPager pager;
+    ViewPagerAdapter adapter;
+    SlidingTabLayout tabs;
+    CharSequence Titles[]={"Home","Events","Tweets"};
+    int Numboftabs =3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
 
-        ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        // Creating The Toolbar and setting it as the Toolbar for the activity
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_my_toolbar);
+        setSupportActionBar(toolbar);
+
+        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles fot the Tabs and Number Of Tabs.
+        adapter =  new ViewPagerAdapter(getSupportFragmentManager(),Titles,Numboftabs);
+
+        // Assigning ViewPager View and setting the adapter
+        pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        // Assiging the Sliding Tab Layout View
+        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        tabs.setDistributeEvenly(true); // To make the Tabs Fixed set this true, This makes the tabs Space Evenly in Available width
+
+        // Setting Custom Color for the Scroll bar indicator of the Tab View
+        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabsScrollColor);
+            }
+        });
+
+        // Setting the ViewPager For the SlidingTabsLayout
+        tabs.setViewPager(pager);
+
     }
 
     @Override
