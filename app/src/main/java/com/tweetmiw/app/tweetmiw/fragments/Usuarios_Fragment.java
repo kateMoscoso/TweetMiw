9449ee3ba9f459e6565c112abe4c10f8702b66b9
Kate Moscoso <katherin.mleon@alumnos.upm.es>
2015-06-24 18:14:31 +0200

    Update de los cambios

--- app/src/main/java/com/tweetmiw/app/tweetmiw/fragments/Usuarios_Fragment.java
+++ app/src/main/java/com/tweetmiw/app/tweetmiw/fragments/Usuarios_Fragment.java
@@ -1,7 +1,9 @@
 package com.tweetmiw.app.tweetmiw.fragments;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v4.app.Fragment;
@@ -12,6 +14,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.tweetmiw.app.tweetmiw.R;
 import com.tweetmiw.app.tweetmiw.adapters.Tweet_Adapter;
@@ -20,8 +23,10 @@ import com.tweetmiw.app.tweetmiw.entities.ProfileUser;
 import com.tweetmiw.app.tweetmiw.entities.Tweet;
 import com.tweetmiw.app.tweetmiw.entities.User;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.Inflater;
 
 import twitter4j.PagableResponseList;
 import twitter4j.Paging;
@@ -30,53 +35,180 @@ import twitter4j.auth.AccessToken;
 
 
 public class Usuarios_Fragment extends Fragment {
+    private WeakReference<FollowersSearchTask> asyncTaskWeakRef;
+    ArrayList<ProfileUser> followers = new ArrayList<ProfileUser>();
+    private FollowersSearchTask mTask;
+    View view;
+    private TaskCallbacks mCallbacks;
     public Usuarios_Fragment(){}
+    interface TaskCallbacks {
+        void onPreExecute();
+        void onProgressUpdate(int percent);
+        void onCancelled();
+        void onPostExecute();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        setRetainInstance(true);
+        startNewAsyncTask();
+        this.view = inflater.inflate(R.layout.fragment_usuarios, container, false);
         return inflater.inflate(R.layout.fragment_usuarios, container, false);
+
+    }
+    private void startNewAsyncTask() {
+        final FollowersSearchTask asyncTask = new FollowersSearchTask(this);
+
+        this.asyncTaskWeakRef = new WeakReference<FollowersSearchTask>(asyncTask);
+
+        asyncTask.execute();
+
+        followers =  asyncTask.getFollowers();
+
+        Log.v("", this.asyncTaskWeakRef.toString() + "status  " + asyncTask.getStatus().toString());
+    }
+    public interface FragmentCallback {
+        public void onTaskDone();
     }
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-        ArrayList<ProfileUser> userArrayList = new ArrayList<ProfileUser>();
-        try {
-
-            twitter4j.Twitter twitter = com.tweetmiw.app.tweetmiw.utils.Properties.getInstance().getTwitter();
-
-            //List< Status> favoritesResources = twitter.getFavorites();
-            int pageno = 1;
-            Paging page = new Paging(pageno++, 10);
-            PagableResponseList<twitter4j.User> users;
-            String screename = twitter.getScreenName();
-            users = twitter.getFollowersList(twitter.getScreenName(),-1);
-            //List<Status> statuses;
-
-
-            User usuario = new User();
-            ProfileUser profileUser = new ProfileUser();
-            for (twitter4j.User user : users) {
-                profileUser.setDescription(user.getDescription());
-                profileUser.setName(user.getName());
-                profileUser.setScreen_name(user.getScreenName());
-                profileUser.setProfile_image_url(user.getProfileImageURL());
-                userArrayList.add(profileUser);
-            }
-
-
-
-        RecyclerView recyclerView = (RecyclerView) getActivity().findViewById(R.id.my_recycler_view_user);
+       RecyclerView recyclerView = (RecyclerView) getActivity().findViewById(R.id.my_recycler_view_user);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setAdapter(new Users_Adapter(userArrayList, R.layout.users_row));
+        recyclerView.setAdapter(new Users_Adapter(followers, R.layout.users_row));
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
-        } catch (twitter4j.TwitterException e) {
-            //  Log.e()
-            Log.e("ss", e.getMessage());
+
+
+    }
+
+    /**
+     * Hold a reference to the parent Activity so we can report the
+     * task's current progress and results. The Android framework
+     * will pass us a reference to the newly created Activity after
+     * each configuration change.
+     *
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mCallbacks = (TaskCallbacks) activity;
+    }
+
+
+    /**
+     * This method will only be called once when the retained
+     * Fragment is first created.
+     *
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Retain this fragment across configuration changes.
+        setRetainInstance(true);
+
+        // Create and execute the background task.
+        mTask = new FollowersSearchTask(this);
+        mTask.execute();
+    }
+
+    /**
+     * Set the callback to null so we don't accidentally leak the
+     * Activity instance.
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mCallbacks = null;
+    }*/
+    private class FollowersSearchTask extends AsyncTask<Object, Void, ArrayList<ProfileUser>> {
+        private WeakReference<Usuarios_Fragment> fragmentWeakRef;
+        private ArrayList<ProfileUser> followers = new ArrayList<ProfileUser>();
+
+        public ArrayList<ProfileUser> getFollowers() {
+            return followers;
         }
+
+        public void setFollowers(ArrayList<ProfileUser> followers) {
+            this.followers = followers;
+        }
+
+        private FollowersSearchTask (Usuarios_Fragment fragment) {
+            this.fragmentWeakRef = new WeakReference<Usuarios_Fragment>(fragment);
+        }
+        private ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setMessage(getResources().getString(R.string.user_loading));
+            progressDialog.show();
+        }
+
+        @Override
+        protected ArrayList<ProfileUser> doInBackground(Object... param) {
+
+
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+            try {
+
+                twitter4j.Twitter twitter = com.tweetmiw.app.tweetmiw.utils.Properties.getInstance().getTwitter();
+
+                PagableResponseList<twitter4j.User> users;
+                String screename = twitter.getScreenName();
+                users = twitter.getFollowersList(screename,-1);
+
+                ProfileUser profileUser = new ProfileUser();
+                for (twitter4j.User user : users) {
+                    profileUser = new ProfileUser();
+                    profileUser.setDescription(user.getDescription());
+                    profileUser.setName(user.getName());
+                    profileUser.setScreen_name(user.getScreenName());
+                    profileUser.setProfile_image_url(user.getProfileImageURL());
+                    followers.add(profileUser);
+                    Log.v("Usuarios",user.getName() + " " + user.getScreenName());
+                }
+           /* do {
+                followers = twitter.getFollowersList("screenName", cursor);
+                for (User follower : followers) {
+                    System.out.println(follower.getName() + " has " + follower.getFollowersCount() + " follower(s)");
+                }
+            } while ((cursor = followers.getNextCursor()) != 0);*/
+
+
+
+            } catch (twitter4j.TwitterException e) {
+                //  Log.e()
+                Log.e("ss", e.getMessage());
+            }
+            return followers;
+        }
+
+
+        protected void onPostExecute(ArrayList<ProfileUser> followers){
+            progressDialog.dismiss();
+            super.onPostExecute(followers);
+
+
+            if (followers.isEmpty()) {
+                Toast.makeText(getActivity(), getResources().getString(R.string.label_tweets_not_found),
+                        Toast.LENGTH_SHORT).show();
+
+            } else {
+                Toast.makeText(getActivity(), getResources().getString(R.string.label_tweets_downloaded),
+                        Toast.LENGTH_SHORT).show();
+                /*getActivity().getLayoutInflater().inflate()
+                RecyclerView recyclerView = (RecyclerView) getActivity().findViewById(R.id.my_recycler_view_user);
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setAdapter(new Users_Adapter(followers, R.layout.users_row));
+                recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                recyclerView.setItemAnimator(new DefaultItemAnimator());*/
+            }
+        }
+
 
     }
 
