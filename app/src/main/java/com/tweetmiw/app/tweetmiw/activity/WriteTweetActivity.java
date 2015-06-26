5360aaa127b86a9c49258780593ece1cf8f1f930
Kate Moscoso <katherin.mleon@alumnos.upm.es>
2015-06-26 13:03:54 +0200

    escribir tweet

--- app/src/main/java/com/tweetmiw/app/tweetmiw/activity/WriteTweetActivity.java
+++ app/src/main/java/com/tweetmiw/app/tweetmiw/activity/WriteTweetActivity.java
@@ -1,16 +1,25 @@
 package com.tweetmiw.app.tweetmiw.activity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tweetmiw.app.tweetmiw.R;
 import com.tweetmiw.app.tweetmiw.utils.SessionManager;
+
+import java.io.File;
+
 import twitter4j.Status;
 import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
@@ -20,6 +29,8 @@ import twitter4j.Twitter;
  */
 public class WriteTweetActivity  extends AppCompatActivity {
     SessionManager session;
+    int RESULT_LOAD_IMAGE = 0;
+    StatusUpdate statusUpdate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,16 +41,18 @@ public class WriteTweetActivity  extends AppCompatActivity {
 
     }
 
+
     private View.OnClickListener buttonClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v){
             try {
-                TextView tv = (TextView) findViewById(R.id.editText);
+                TextView tv = (TextView) findViewById(R.id.tweet);
                 String mensaje = tv.getText().toString();
                 StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
                 StrictMode.setThreadPolicy(policy);
                 Twitter twitter = session.getTwitter();
-                StatusUpdate statusUpdate = new StatusUpdate(mensaje);
+                statusUpdate = new StatusUpdate(mensaje);
+
                 Status status = twitter.updateStatus(statusUpdate);
                 if(status.getCreatedAt().toString().length()>0){
                     Toast.makeText(getApplication(), getResources().getString(R.string.label_tweet_published),
@@ -60,6 +73,25 @@ public class WriteTweetActivity  extends AppCompatActivity {
     public void cerrar(View v) {
         onBackPressed();
     }
+    public  void seleccionarImagen(View v){
+        Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
+    }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            statusUpdate.setMedia(new File(picturePath));
+           // ImageView imageView = (ImageView) findViewById(R.id.imgView);
+            //imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
+    }
 }
